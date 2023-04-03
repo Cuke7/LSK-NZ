4c4c36d09e34dc0c32fea6261c4e6f7edf2f398d
Louis <cassany.louis@gmail.com>
2023-04-03 10:31:29 +0200

    basic layout

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+    <div data-theme="corporate" class="h-screen flex flex-col">
+        <!-- <div class="flex bg-blue-400 justify-center items-center font-mono">Layout</div> -->
+        <slot />
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex h-full flex-1 flex-col">
+        <div class="flex h-3/5 w-full">
+            <div class="bg-green-400 flex items-center w-2/3 justify-center font-mono">Sketch</div>
+            <div class="bg-slate-400 flex items-center flex-1 justify-center font-mono">Modes</div>
+        </div>
+        <div class="flex flex-1 bg-red-400 justify-center items-center font-mono">Mesures</div>
+    </div>
+</template>
